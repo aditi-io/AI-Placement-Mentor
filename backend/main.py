e19ac8425e4723c656e6665d4b85788b197bb6f7
Aditi io <maharoraditi@gmail.com>
2026-07-09 13:32:25 +0530

    Day 5: Added skill analysis and roadmap generation

--- backend/main.py
+++ backend/main.py
@@ -2,15 +2,14 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 class Student(BaseModel):
     name:str
-    cgpa:float
     college:str
+    cgpa:float
     skills:list[str]
 
 app = FastAPI()
 
 @app.get("/")
 def home():
-    
     return {"message": "Welcome to AI Placement Mentor"}
 @app.get("/profile")
 def profile():
@@ -28,12 +27,41 @@ def skills():
                     "AI"
                     ]
     }
-@app.post("/student")
-def add_student(student:Student):
+@app.post("/profile")
+def create_profile(student:Student):
+    if student.cgpa>=8.5:
+        level = "Excellent"
+    elif student.cgpa>=7:
+        level = "Good"
+    else:
+        level = "Needs Improvement"
+    required=["Python","SQL","DSA"]
+
+    missing=[]
+    for skill in required:
+        if skill not in student.skills:
+            missing.append(skill)
+
+    roadmap = []
+
+    for skill in missing:
+        if skill == "Python":
+            roadmap.append("Complete Python Fundamentals")
+
+        elif skill == "SQL":
+            roadmap.append("Learn SQL Joins and Queries")
+
+        elif skill == "DSA":
+            roadmap.append("Practice Striver A2Z Sheet")
+            
     return {
-        "message":"student added successfully",
-        "student":student
-        
+        "name" : student.name,
+        "college" : student.college,
+        "cgpa" : student.cgpa,
+        "skills" : student.skills,
+        "level" : level,
+        "missing_skill" : missing,
+        "roadmap" : roadmap
     }
 @app.get("/student/{name}/{cgpa}")
 def get_student(name: str,cgpa:float):
